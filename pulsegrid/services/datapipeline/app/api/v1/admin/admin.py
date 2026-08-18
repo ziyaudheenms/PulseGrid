f3b1ec967181674e6e95242347b1babce88fcb44
ziyaudheenms <msharafudeen193@gmail.com>
2026-08-18 22:03:27 +0530

    added the test API for scrapper

--- pulsegrid/services/datapipeline/app/api/v1/admin/admin.py
+++ pulsegrid/services/datapipeline/app/api/v1/admin/admin.py
@@ -1,12 +1,16 @@
+import json
+
 import structlog
+from pydantic import TypeAdapter    #Type Adapter is used to convert into pydantic modelss
 from fastapi import APIRouter, Depends, HTTPException, status, Response
 import redis.asyncio as redis_asyncio
 from pymongo.asynchronous.database import AsyncDatabase
 
 
+from pulseBot.scrapper import PulseBotScrapper
 from core.database import get_database
 from pulseBot.crawler import PulseBotCrawler
-from schema.source_schema import SourceIds, SourceResponceSchema, SourceSchema, SourceCollection
+from schema.source_schema import SourceIds, SourceResponceSchema, SourceSchema, SourceCollection, CrawlObjectSchema
 from service.sourceService import SourceService
 from api.v1.deps import get_redis_client, get_source_service
 from core.logging import setup_logging
@@ -17,6 +21,7 @@ logger = structlog.get_logger()
 
 router = APIRouter(prefix="/admin", tags=["admin"])
 cache_key = "pulsegrid:sources:all"
+crawler_cache_key = "pulsegrid:sources:crawl:all"
 
 @router.post("/source", status_code=status.HTTP_201_CREATED,response_model=ApiResponseSchema[SourceIds])
 async def create_source(
@@ -92,6 +97,47 @@ async def implement_crawler(
     
 
     crawl_responce = await pulsebot.fetch_urls(parsed_sources.data.sources)
+
+    if all(crawl_responce):
+        return ApiResponseSchema(
+            status_code=200,
+            message=f"Completed the crawl function",
+            data='successfully ran the crawler'
+        )
+    else:
+        raise HTTPException(
+                    status_code=status.HTTP_409_CONFLICT,
+                    detail=f"some sources are not crawlable....."
+        )
+
+
+
+@router.get("/scrapper", response_model=ApiResponseSchema[str])
+async def implement_scrapper(
+    db: AsyncDatabase = Depends(get_database),
+    redis_client: redis_asyncio.Redis | None = Depends(get_redis_client)
+):
+    cached_crawl_source = await redis_client.get(crawler_cache_key) if redis_client else None
+    
+    if not cached_crawl_source:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Cached resources not found......."
+        )
+
+    #Initiallzing our crawler bot for crawling
+    pulsebot = PulseBotScrapper(
+        db = db,
+        redis=redis_client,
+        concurrency_limit=5,
+        max_retries=3,
+    )
+
+    parsed_list_of_crawled_objects = await redis_client.lrange(crawler_cache_key, 0, -1) #this converts the stringified responce into python objects -> we want to pass the parsed_source.sources (contains the list of SourceResponceSchema)
+    parsed_list_of_crawled_objects_adapter = TypeAdapter(list[CrawlObjectSchema]) #the adapter class to which we have to convert the list of bytes into
+    parsed_list_of_crawled_objects_adapter_python_formatted = parsed_list_of_crawled_objects_adapter.validate_python([json.loads(crawl_object_json) for crawl_object_json in parsed_list_of_crawled_objects])
+
+    crawl_responce = await pulsebot.load_the_collection(crawlObjects=parsed_list_of_crawled_objects_adapter_python_formatted)
 
     if all(crawl_responce):
         return ApiResponseSchema(
